420a49bd41008ca2c28b7c21aee416e38cfbd9d7
Mathieu <bon.mathieu@gmail.com>
2024-01-09 23:37:52 +0100

    Created TextField component as custom TextInput

---
+++ components/TextField.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StyleSheet, TextInput } from 'react-native'
+
+export default function TextField({ value, placeholder, onChangeText, autoCapitalize, secureTextEntry }) {
+
+    return (
+
+        <TextInput
+            style={styles.field}
+            placeholder={placeholder}
+            value={value}
+            onChangeText={onChangeText}
+            autoCorrect={false}
+            autoCapitalize={autoCapitalize}
+            secureTextEntry={secureTextEntry}
+        />
+
+    )
+}
+
+const styles = StyleSheet.create({
+    field: {
+        height: 40,
+        width: '85%',
+        margin: 12,
+        borderWidth: 1,
+        borderColor: 'gray',
+        padding: 10,
+    }
+})
