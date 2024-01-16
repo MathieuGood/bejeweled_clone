73d1a9ae99414efae9035619b3501ea656e2582f
Mathieu <bon.mathieu@gmail.com>
2024-01-16 21:10:37 +0100

    New functions to get ranking

--- SQLfunctions.sql
+++ SQLfunctions.sql
@@ -35,3 +35,82 @@ BEGIN
 END //
 
 DELIMITER ;
+
+
+-- TOP SCORES
+
+SELECT games.player_id, player_name, score, prev_ranking FROM games INNER JOIN players ON games.player_id = players.player_id ORDER BY score DESC LIMIT 6;
+
+
+
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE getTopScores()
+BEGIN
+    SET @count := 0;
+
+        DROP TEMPORARY TABLE IF EXISTS temp_top_scores;
+
+    CREATE TEMPORARY TABLE temp_top_scores AS
+    SELECT (@count:=@count+1) AS rank, games.player_id, player_name, score, prev_ranking
+    FROM games
+    INNER JOIN players ON games.player_id = players.player_id
+    ORDER BY score DESC
+    LIMIT 6;
+
+    SELECT * FROM temp_top_scores;
+
+
+END //
+
+DELIMITER ;
+
+
+
+-- UPDATE RANKING
+
+UPDATE players SET prev_ranking = 1 WHERE player_id = 1
+
+
+
+
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE updateRanking()
+BEGIN
+
+
+    SET @count := 1;
+
+    UPDATE player SET prev_ranking = NULL;
+
+
+    WHILE count < (SELECT player_id FROM players ORDER BY player_id DESC LIMIT 1) DO
+
+        DECLARE @playerRank INT;
+        CALL getPlayerRank(count, @playerRank);
+
+        UPDATE players SET prev_ranking = @playerRank WHERE player_id = @count;
+
+        SET count = count + 1;
+
+
+    END WHILE;
+
+
+END //
+
+DELIMITER ;
+
+
+
+
+
+
+
+-----
+
+
