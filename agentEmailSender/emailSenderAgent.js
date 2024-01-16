c3ef84c8d25688d5913cdecff0b13d6a4a1d6324
Mathieu <bon.mathieu@gmail.com>
2024-01-16 22:12:20 +0100

    Initial commit for agentEmailSender

---
+++ agentEmailSender/emailSenderAgent.js
@@ -0,0 +1,223 @@
+const Mailjet = require('node-mailjet')
+
+
+sendUpdatesToAllPlayers()
+
+
+
+function sendUpdatesToAllPlayers() {
+
+    return fetch(`http://mathieubon.com:3001/playerlist`, {
+        method: 'GET',
+        headers: { "Content-Type": "application/json" }
+    })
+        .then(response => response.json())
+        .then(json => {
+            // Get the list of all unique player IDs in the databse
+            if (json) {
+                // console.log(json)
+                // For each player in the database
+                json.forEach((player) => {
+                    // Run function to send recap by e-mail if last played game has ended for an hour at least
+                    sendRecapEmailIfTimeReached(player.player_id)
+                })
+                // return json
+
+            } else {
+                console.log('No players in the database')
+                return null
+            }
+        })
+        .catch(error => {
+            console.error(error)
+            return null
+        });
+}
+
+
+
+function sendMail(player_email, player_name, fetchScoresResult) {
+
+    const mailjet = Mailjet.apiConnect(
+        // API Key
+        'abe539777eedf22676deaf4d3643a0a9',
+        // Secret Key
+        '125ff23dba0087e2a32070553111bca5')
+
+    // console.log(fetchScoresResult)
+
+    let scoresHTMLContent = ''
+    let scoresTextContent = ''
+    fetchScoresResult.forEach((game) => {
+        const formattedDate = new Date(game.end_time).toLocaleDateString('fr-FR', {
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
+        })
+
+        scoresHTMLContent += '<tr><td>' + formattedDate + '</td><td>' + game.score + '</td><tr>'
+        scoresTextContent += formattedDate + ' || ' + game.score + ' points\n'
+
+    })
+
+    const emailToSend = {
+        Messages: [
+            {
+                From: {
+                    Email: "mathieu.bon@uha.fr",
+                    Name: "Bejeweled Clone"
+                },
+                To: [
+                    {
+                        Email: player_email,
+                        Name: player_name
+                    }
+                ],
+                Subject: "Bejeweled Clone 💎 Last games recap",
+                TextPart:
+                    `Dear ${player_name},
+                    
+                    Thank you for playing Bejeweled Clone 💎
+                    
+                    Here is a recap of your last games :` +
+
+                    scoresTextContent +
+
+                    `See you soon 👋
+                    
+                    Bejeweled Clone Team`,
+                HTMLPart:
+                    `<p>Dear ${player_name},</p>` +
+                    `<p>Thank you for playing Bejeweled Clone 💎</p>` +
+                    `<p>Here is a recap of your last games :</p>` +
+                    `<table><thead><tr><th>Date and time</th><th>Score</th></tr></thead>` +
+                    `<tbody>` +
+                    scoresHTMLContent +
+                    `</tbody></table>` +
+                    `<p>See you soon 👋<p>` +
+                    `<p>Bejeweled Clone team</p>`
+            }
+        ]
+    }
+
+    const sendMailjet = mailjet
+        .post('send', { version: 'v3.1' })
+        .request(emailToSend)
+
+
+    sendMailjet.then((result) => {
+        console.log('Sending e-mail to ' + player_name + ' at ' + player_email)
+        console.log(result.body)
+    }).catch((err) => {
+        console.log(err.statusCode)
+    })
+
+}
+
+
+
+function getLastGames(player_id) {
+    return fetch(`http://mathieubon.com:3001/lastgames/${player_id}`, {
+        method: 'GET',
+        headers: { "Content-Type": "application/json" }
+    })
+        .then(response => response.json())
+        .then(json => {
+            if (json) {
+                return json
+            } else {
+                console.log('No last games')
+                return null
+            }
+        })
+        .catch(error => {
+            console.error(error)
+            return null
+        });
+}
+
+
+
+function updateLastGameId(player_id, last_game_id) {
+
+    const bodyData = {
+        player_id: player_id,
+        last_game_id: last_game_id
+    }
+
+    return fetch(`http://mathieubon.com:3001/updatelastgame`, {
+        method: 'POST',
+        body: JSON.stringify(bodyData),
+        headers: { "Content-Type": "application/json" }
+    })
+        .then(response => response.json())
+        .then(json => {
+            if (json) {
+                console.log('Updated last_game_id for player ' + player_id, json);
+            } else {
+                console.log('Could not add last_game_id to player ' + player_id)
+            }
+        })
+        .catch(error => {
+            console.error(error)
+            return null
+        });
+}
+
+
+
+function sendRecapEmailIfTimeReached(player_id) {
+    // Get all the last games for one player
+    getLastGames(player_id)
+        .then(result => {
+            if (result != false) {
+
+                // console.log(result)
+
+                // Get current datetime
+                let now = new Date()
+                // Get end_time of last game played
+                let end_time = new Date(result[result.length - 1]['end_time'])
+                // Add one hour to the end_time of last game played
+                end_time.setTime(end_time.getTime() + 60 * 60 * 1000)
+
+                // console.log('END : ' + end_time)
+                // console.log('NOW : ' + now)
+
+                // If it is time to notify the user
+                if (end_time < now) {
+
+                    const email = result[result.length - 1]['player_email']
+                    const name = result[result.length - 1]['player_name']
+
+                    // Send e-mail
+                    // console.log('SEND E-MAIL NOW to ' + email)
+                    sendMail(email, name, result)
+
+
+                    // Update last_game_id in player table
+                    // const last_game_id = result[result.length - 1]['game_id']
+                    // updateLastGameId(player_id, last_game_id)
+                    // console.log('-> Update last_game_id in player table')
+
+
+                } else {
+                    // Print how many minutes to wait in console
+                    let waitTime = new Date(end_time - now)
+                    waitTime = waitTime.getUTCMinutes()
+                    console.log('DO NOT SEND E-MAIL NOW to ' + email)
+                    console.log('-> Minutes to wait before sending : ' + waitTime)
+                }
+
+            } else {
+                console.log('NOTHING TO SEND to player ID ' + player_id)
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        })
+}
+
+
