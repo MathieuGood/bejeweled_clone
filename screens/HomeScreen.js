6f5c0ec53e0bd2ac1fff5176b1dc9ee20ae6a68a
Mathieu <bon.mathieu@gmail.com>
2024-01-18 15:43:53 +0100

    Included new styling for HomeScreen and RegisterScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,70 +1,117 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, View, Text, SafeAreaView, Alert } from 'react-native'
+import { StyleSheet, View, SafeAreaView, ImageBackground } from 'react-native'
 import TouchButton from '../components/TouchButton'
 import TextField from '../components/TextField'
+import Header from '../components/Header'
 import { checkCredentials } from '../core/apiRequests'
 
 export default function HomeScreen({ navigation }) {
 
     const [state, setState] = useState({
-        theme: 'light',
         email: '',
         password: ''
     })
 
-    const { theme, email, password } = state
+    const { email, password } = state
 
     // For debug : know when component renders
     // useEffect(() => {
     //     console.log('Component HomeScreen mounted')
     // })
 
+    // QUESTIONS
+    // - Move component styles to each component file ?
+
 
     return (
-        <SafeAreaView style={styles.mainContainer}>
+        <View style={styles.mainContainer}>
 
-            <Text>Bejeweled Clone HomeScreen</Text>
+            <ImageBackground
+                source={require('../assets/essai3.png')}
+                style={styles.background}
+                resizeMode='cover'
+            >
+                <View style={styles.form}>
 
-            <TextField
-                placeholder='E-mail'
-                value={email}
-                onChangeText={text => setState(prevState => {
-                    return { ...prevState, email: text }
-                })}
-                autoCapitalize='none'
-                secureTextEntry={false}
-            />
-            <TextField
-                placeholder='Password'
-                value={password}
-                onChangeText={text => setState(prevState => {
-                    return { ...prevState, password: text }
-                })}
-                autoCapitalize='none'
-                secureTextEntry={true}
-            />
+                    <Header style={styles.header} title='Welcome' />
 
-            <TouchButton title='Login' press={() => {
-                // On click, check if e-mail and password match
-                checkCredentials(email, password, navigation)
-            }}
-            />
+                    <TextField
+                        placeholder='E-mail'
+                        value={email}
+                        onChangeText={text => setState(prevState => {
+                            return { ...prevState, email: text }
+                        })}
+                        autoCapitalize='none'
+                        secureTextEntry={false}
+                    />
+                    <TextField
+                        placeholder='Password'
+                        value={password}
+                        onChangeText={text => setState(prevState => {
+                            return { ...prevState, password: text }
+                        })}
+                        autoCapitalize='none'
+                        secureTextEntry={true}
+                    />
 
-            <TouchButton title='Create account' press={() => {
-                navigation.navigate('RegisterScreen')
-            }}
-            />
+                    <TouchButton
+                        title='Login'
+                        press={() => {
+                            // On click, check if e-mail and password match
+                            checkCredentials(email, password, navigation)
+                        }}
+                    />
 
-        </SafeAreaView>
+                    <TouchButton
+                        title='Create account'
+                        press={() => {
+                            navigation.navigate('RegisterScreen')
+                        }}
+                    />
+
+                </View>
+
+            </ImageBackground>
+
+        </View>
     )
 
 }
 
 let styles = StyleSheet.create({
     mainContainer: {
-        backgroundColor: 'lightgray',
+        flex: 1,
+        // alignItems: 'center',
+        // justifyContent: 'center'
+    },
+    background: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
-    }
+    },
+    form: {
+        backgroundColor: 'rgba(244, 232, 193, 0.8)',
+        padding: 20,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#d4af37', // Doré
+        width: '80%',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        alignItems: 'center',
+    },
+    header: {
+        color: '#2b50c8',
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        marginBottom: 20
+    },
 })
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, SafeAreaView, View, Text, Alert } from 'react-native'
+import { StyleSheet, ImageBackground, View, Alert } from 'react-native'
 import TouchButton from '../components/TouchButton'
 import TextField from '../components/TextField'
 import { addPlayer, checkIfEmailDoesNotExist } from '../core/apiRequests'
@@ -14,18 +14,19 @@ export default function RegisterScreen({ navigation }) {
         password: ''
     })
 
-    const { theme, name, email, password } = state
+    const { name, email, password } = state
 
-    
+
     //////////
     // TO DO
     // Implement alert if checkNameFormat is false
     //                 if checkPasswordFormat is false
     //                 if checkEmailFormat is false
+    // Global alert or individual alerts for each error ?
+    //
     /////////
 
-    // Checking if name, email and password have right format
-
+    // Check if name, email and password have right format
     function checkUserEntryAndAddPlayer(name, email, password) {
         checkIfEmailDoesNotExist(email, () => {
             console.log('EMAIL TES$T OK')
@@ -33,7 +34,8 @@ export default function RegisterScreen({ navigation }) {
                 && checkPasswordFormat(password)
                 && checkNameFormat(name)) {
                 console.log('Adding player')
-                    // Then add new player to database and navigate to PlayerScreen
+                // If it is the case, execute addPlayer() that sends API call to create new player in database
+                // Navigate to PlayerScreen
                 addPlayer(name.trim(), email, password, navigation)
             }
         })
@@ -42,50 +44,88 @@ export default function RegisterScreen({ navigation }) {
 
 
     return (
-        <SafeAreaView style={styles.mainContainer}>
+        <View style={styles.mainContainer}>
 
-            <Text>Register screen</Text>
-            <TextField
-                placeholder='Name'
-                value={name}
-                onChangeText={text => setState(prevState => {
-                    return { ...prevState, name: text }
-                })}
-                autoCapitalize='words'
-                secureTextEntry={false}
-            />
-            <TextField
-                placeholder='E-mail'
-                value={email}
-                onChangeText={text => setState(prevState => {
-                    return { ...prevState, email: text }
-                })}
-                autoCapitalize='none'
-                secureTextEntry={false}
-            />
-            <TextField
-                placeholder='Password'
-                value={password}
-                onChangeText={text => setState(prevState => {
-                    return { ...prevState, password: text }
-                })}
-                autoCapitalize='none'
-                secureTextEntry={true}
-            />
-            <TouchButton
-                title='Create account'
-                press={() => checkUserEntryAndAddPlayer(name, email, password)}
-            />
+            <ImageBackground
+                source={require('../assets/RegisterBK2.png')}
+                style={styles.background}
+                resizeMode='cover'
+            >
 
-        </SafeAreaView>
+                <View style={styles.form}>
+                    {/* TextField behavior :
+                    onChangeText -> update state with user entry and reinject the state as value */}
+                    <TextField
+                        placeholder='Name'
+                        value={name}
+                        onChangeText={text => setState(prevState => {
+                            return { ...prevState, name: text }
+                        })}
+                        autoCapitalize='words'
+                        secureTextEntry={false}
+                    />
+                    <TextField
+                        placeholder='E-mail'
+                        value={email}
+                        onChangeText={text => setState(prevState => {
+                            return { ...prevState, email: text }
+                        })}
+                        autoCapitalize='none'
+                        secureTextEntry={false}
+                    />
+                    <TextField
+                        placeholder='Password'
+                        value={password}
+                        onChangeText={text => setState(prevState => {
+                            return { ...prevState, password: text }
+                        })}
+                        autoCapitalize='none'
+                        secureTextEntry={true}
+                    />
+                    <TouchButton
+                        title='Create account'
+                        press={() => checkUserEntryAndAddPlayer(name, email, password)}
+                    />
+
+                    <TouchButton
+                        title='Back to home page'
+                        press={() => {
+                            navigation.navigate('HomeScreen')
+                        }}
+                    />
+
+                </View>
+
+            </ImageBackground>
+
+        </View>
     )
 }
 
 let styles = StyleSheet.create({
     mainContainer: {
-        backgroundColor: 'lightgray',
+        flex: 1,
+    },
+    background: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
-    }
+    },
+    form: {
+        backgroundColor: 'rgba(244, 232, 193, 0.8)',
+        padding: 20,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#d4af37', // Doré
+        width: '80%',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        alignItems: 'center',
+    },
 })
