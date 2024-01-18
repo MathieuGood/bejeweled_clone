cd68f57d75efd7ec63950ab5de5a95b83a8db521
Mathieu <bon.mathieu@gmail.com>
2024-01-18 15:42:14 +0100

    Edited styling on components

--- components/TouchButton.js
+++ components/TouchButton.js
@@ -1,30 +1,35 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 export default function TouchButton({ title, press }) {
 
     return (
-        <TouchableOpacity style={styles.buttonContainer} onPress={press}>
-            <Text style={styles.buttonText}>{title}</Text>
-        </TouchableOpacity>
+        <View>
+            <TouchableOpacity style={styles.buttonContainer} onPress={press}>
+                <Text style={styles.buttonText}>{title}</Text>
+            </TouchableOpacity>
+        </View>
     )
 }
 
 
 const styles = StyleSheet.create({
     buttonText: {
-        color: '#FF004D',
-        alignSelf: 'center',
-        fontWeight: '800',
-        fontSize: 14,
-        letterSpacing: 0.9
+        color: '#e8b923',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        //fontFamily: 'Egyptian Hieroglyphs Bold' (voir comment l'intégrer cf module caméra, la logique est à peu près identique)
     },
     buttonContainer: {
+        backgroundColor: '#2b50c8',
+        alignItems: 'center',
         justifyContent: 'center',
-        width: '85%',
-        borderRadius: 5,
-        backgroundColor: '#FAEF5D',
-        paddingVertical: 10,
-        marginVertical: 5
+        padding: 10,
+        borderRadius: 25,
+        width: 250,
+        height: 61,
+        alignSelf: 'center',
+        margin: 5
     }
 })
