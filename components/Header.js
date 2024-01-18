0862c1ad5d53d65bd36f8844fa634f2c6f0228eb
Mathieu <bon.mathieu@gmail.com>
2024-01-18 15:42:34 +0100

    Header component

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import  React from 'react';
+import {Text, View} from 'react-native';
+
+
+    export default class Header extends React.Component {
+        render() {
+          const { title, style } = this.props;
+          return (
+            <View>
+              <Text style={style}>{title}</Text>
+            </View>
+          );
+        }
+      }
+      
+
+
