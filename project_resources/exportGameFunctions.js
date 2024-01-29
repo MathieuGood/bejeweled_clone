57ab99d272dbe7cd7e317f24d3c31c167db9f4dd
Mathieu <bon.mathieu@gmail.com>
2024-01-29 23:01:32 +0100

    Succesfully implemented grid creation with no matches. Cell swap showing on UI but also happening on deep copy of grid after tests.

--- project_resources/exportGameFunctions.js
+++ project_resources/exportGameFunctions.js
@@ -19,6 +19,7 @@
 export const buildGameGridWithNoMatches = (size, numberOfDifferentValues) => {
 
     while (true) {
+        console.log('Generating grid...')
         gameGrid = buildGameGrid(size, numberOfDifferentValues)
         if (checkGameGridForAlignments(gameGrid) == '') {
             return gameGrid
@@ -35,7 +36,7 @@ export const buildGameGridWithNoMatches = (size, numberOfDifferentValues) => {
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-export const buildGameGrid = (size, numberOfDifferentValues) => {
+const buildGameGrid = (size, numberOfDifferentValues) => {
 
     let gameGrid = []
 
@@ -49,4 +50,196 @@ export const buildGameGrid = (size, numberOfDifferentValues) => {
     }
 
     return gameGrid
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Check if there are any alignments of matching items on the game grid
+// Return an array for each alignement with coordinates of each matching cell
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export const checkGameGridForAlignments = (gameGrid) => {
+
+    let allMatches = []
+
+    for (let i = 0; i < gameGrid.length; i++) {
+
+        checkForMatchesOneWay(gameGrid, 'row', i, allMatches)
+        checkForMatchesOneWay(gameGrid, 'col', i, allMatches)
+    }
+    console.log('Matches found :')
+    console.log(allMatches)
+    return allMatches
+
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Check for matches one way (x or y axis)
+//
+// direction parameter : 'row' or 'col'
+// i : counter of the loop the function is inserted  in
+// allMatches : array to return all results in
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const checkForMatchesOneWay = (gameGrid, direction, index, allMatches) => {
+
+    // previousItem records what item was before current entry
+    let previousItem = ''
+
+    // count records the number of matching items 
+    let count = 1
+
+    // matches records the coordinates of matching items
+    let matches = []
+
+    // Loop through rows/columns of gameGrid
+    for (let j = 0; j < gameGrid.length; j++) {
+
+        // Intialize variable for value of the item
+        let value = ''
+
+        // y/x coordinates of previous item
+        let previousCoordinates = ''
+
+        // y/x coordinates of current item
+        let currentCoordinates = ''
+
+        // Set coordinates accordingly for the result regarding the direction chosen
+        if (direction == 'row') {
+            value = gameGrid[index][j]
+            previousCoordinates = [index, j - 1]
+            currentCoordinates = [index, j]
+        } else {
+            value = gameGrid[j][index]
+            previousCoordinates = [j - 1, index]
+            currentCoordinates = [j, index]
+        }
+
+        // If it is the first occurence of the list, initalize previousItem to current value
+        if (previousItem === '') {
+            previousItem = value
+        } else {
+            // For all other items
+            // Check if current value matches previous item
+            if (value === previousItem) {
+                // In case of a match, add the coordinates of the previous (first) item of the row/column to match array
+                if (count === 1) {
+                    matches.push(previousCoordinates)
+                }
+                // Add the coordinates of current item to match array
+                matches.push(currentCoordinates)
+                // Increase match count 
+                count++
+                // If the value is the last item in the row/column and is winning match, add coordinates to matches
+                if (j === gameGrid.length - 1 && count > 2) {
+                    allMatches.push(matches)
+                }
+            } else {
+                // If the value does not match and if count is at least 3, record matches to allMatches array
+                if (count > 2) {
+                    allMatches.push(matches)
+                }
+                // Reset matches and count to default values
+                matches = []
+                count = 1
+            }
+            // Set previousItem to current value before next iteration
+            previousItem = value
+        }
+    }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Display and color the game grid
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export const showGameGrid = (gameGrid) => {
+    console.log('')
+    console.log('    0 1 2 3 4 5 6 7')
+    console.log('    - - - - - - - -')
+
+    gameGrid.forEach((row, index) => {
+        rowDisplay = ''
+        for (let value of row) {
+            let colorCode = getColorCode(value)
+            let coloredValue = `\x1b[${colorCode}m${value}\x1b[0m`
+            rowDisplay += (value !== '') ? coloredValue + ' ' : '* '
+        }
+        console.log(index + ' | ' + rowDisplay)
+    })
+    console.log('')
+}
+
+function getColorCode(value) {
+    // You can customize this function to assign different color codes based on the integer value
+    switch (value) {
+        case 1:
+            return 31 // Red
+        case 2:
+            return 32 // Green
+        case 3:
+            return 33 // Yellow
+        case 4:
+            return 34 // Blue
+        case 5:
+            return 35 // Magenta
+        case 6:
+            return 36 // Cyan
+        case 7:
+            return 37 // White
+        // Add more cases for additional integer values and corresponding colors
+        default:
+            return 0 // Default color (reset)
+    }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Check if coordinates are not out of the grid
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function isValidCoordinate(gameGrid, coord) {
+    const gridSize = gameGrid.length
+    return coord[0] >= 0 && coord[0] < gridSize && coord[1] >= 0 && coord[1] < gridSize
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Swap two items (coordinatesItem1, coordinatesItem2) on the grid
+// Returns the updated gameGrid
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export const swapTwoItemsOnGrid = (gameGrid, [y1, x1], [y2, x2]) => {
+
+    // Check if the coordinates are within the grid bounds
+    if (isValidCoordinate(gameGrid, [y1, x1]) && isValidCoordinate(gameGrid, [y2, x2])) {
+        // Swap values between two items
+        const temp = gameGrid[y1][x1]
+        gameGrid[y1][x1] = gameGrid[y2][x2]
+        gameGrid[y2][x2] = temp
+        // console.log(`Swapping ${gameGrid[y1][x1]} and ${gameGrid[y2][x2]}`)
+        // Return the updated gameGrid after switch
+        return gameGrid
+    } else {
+        // Return false if move is out of the grid
+        // console.log(`! Out of the grid move: cannot swap [${y1}, ${x1}] with [${y2}, ${x2}]`)
+        return false
+    }
 }
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,52 +1,119 @@
-import React, { useState } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, Text, Alert } from 'react-native'
 import TouchButton from '../components/TouchButton'
 import GameGrid from '../components/GameGrid'
-import { buildGameGrid } from '../project_resources/exportGameFunctions'
+import {
+    buildGameGridWithNoMatches,
+    showGameGrid,
+    checkGameGridForAlignments,
+    swapTwoItemsOnGrid
+} from '../project_resources/exportGameFunctions'
 
 
 
 export default function GameScreen({ navigation }) {
 
-    const exampleGrid = [
-        [4, 5, 6, 6, 5, 1, 7, 2],
-        [1, 5, 6, 7, 7, 1, 3, 3],
-        [1, 3, 6, 0, 4, 7, 2, 1],
-        [0, 5, 6, 7, 7, 0, 3, 0],
-        [2, 0, 3, 3, 1, 2, 6, 1],
-        [0, 7, 2, 3, 2, 6, 6, 1],
-        [5, 4, 0, 3, 0, 2, 2, 1],
-        [7, 4, 6, 4, 1, 0, 5, 4],
-    ]
-
-
-    const [gameGrid, setGameGrid] = useState(buildGameGrid(8, 8))
+    // const exampleGrid = [
+    //     [4, 5, 6, 6, 5, 1, 7, 2],
+    //     [1, 5, 6, 7, 7, 1, 3, 3],
+    //     [1, 3, 6, 0, 4, 7, 2, 1],
+    //     [0, 5, 6, 7, 7, 0, 3, 0],
+    //     [2, 0, 3, 3, 1, 2, 6, 1],
+    //     [0, 7, 2, 3, 2, 6, 6, 1],
+    //     [5, 4, 0, 3, 0, 2, 2, 1],
+    //     [7, 4, 6, 4, 1, 0, 5, 4],
+    // ]
     // const [gameGrid, setGameGrid] = useState(exampleGrid)
 
-    const [firstPress, setFirstPress] = useState([null, null])
-    const [secondPress, setSecondPress] = useState([null, null])
+    // Build a 8*8 grid with no matches
+    const [gameGrid, setGameGrid] = useState(() => buildGameGridWithNoMatches(8, 8))
+
+    // Set the number of attempts to 3
+    const [attempts, setAttempts] = useState(3)
+
+    // Set states to record user entry
+    const [firstPress, setFirstPress] = useState(null)
+    const [secondPress, setSecondPress] = useState(null)
+    const [lastPress, setLastPress] = useState(null)
 
 
-    
+
 
     const getCellCoordinates = (row, col) => {
-        console.log(`Cell pressed: ${row}, ${col}`)
+        console.log(`\nPRESS: row ${row}, col ${col}`)
 
         if (firstPress == [row, col]) {
             console.log("Same cell as before!")
         }
-        console.log("firstPress before updating state : " + firstPress)
-        setFirstPress([row, col])
 
-        console.log("firstPress after updating state : " + firstPress)
+        // console.log("firstPress before updating state : " + firstPress)
+        setLastPress([row, col])
 
     }
+
+    // Record last cell pressed in either firstPress or secondPress
+    useEffect(() => {
+
+        // let grid = gameGrid
+        let grid = JSON.parse(JSON.stringify(gameGrid));
+
+        console.log("useEffect : lastPress is " + lastPress)
+        if (firstPress === null) {
+            setFirstPress(lastPress)
+            console.log("useEffect : firstPress recorded >>> " + lastPress)
+        } else {
+            if (secondPress === null) {
+                setSecondPress(lastPress)
+                console.log("useEffect : secondPress recorded >>> " + lastPress)
+
+                console.log("firstPress is " + firstPress)
+                console.log("secondPress is " + lastPress)
+
+                // Player has selected two cells
+                // NOW DO SOMETHING !
+                //
+                showGameGrid(grid)
+
+                swapTwoItemsOnGrid(grid, firstPress, lastPress)
+                checkGameGridForAlignments(grid)
+                showGameGrid(grid)
+                setGameGrid(grid)
+
+                setFirstPress(null)
+                setSecondPress(null)
+
+                if (firstPress.toString() === lastPress.toString()) {
+                    console.log("CELLS ARE THE SAME!!!!!!")
+
+                    // Decrement attempts
+                    setAttempts(attempts - 1)
+                    // If it is the last attempt, show alert and stop game
+                    if (attempts - 1 === 0) {
+                        Alert.alert(
+                            'End of game',
+                            'Loser! Game is over!',
+                            [
+                                { text: 'OK' }
+                            ],
+                            { cancelable: false }
+                        );
+                        navigation.navigate('PlayerScreen')
+                    }
+                }
+
+            }
+        }
+    }, [lastPress])
 
 
     return (
         <View style={styles.mainContainer}>
 
             <Text>GameScreen</Text>
+            <Text>Number of tries left : {attempts}</Text>
+            <Text></Text>
+            <Text>firstPress : {firstPress}</Text>
+            <Text>secondPress : {secondPress}</Text>
 
             <TouchButton
                 title='Back to player screen'
