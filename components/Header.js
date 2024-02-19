d2c517d0fb8ba1c3da804869a5b8be6cb3b287fb
Mathieu <bon.mathieu@gmail.com>
2024-02-19 10:20:00 +0100

    Added styling directly into component.

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 
-export default function Header({ title, style }) {
+export default function Header({ title}) {
 
     return (
 
       <View>
-        <Text style={style}>{title}</Text>
+        <Text style={styles.header}>{title}</Text>
       </View>
 
     )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    color: '#2b50c8',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    marginBottom: 20
+},
+})
+
