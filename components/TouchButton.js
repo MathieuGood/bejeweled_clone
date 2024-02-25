40b28f81b2c1000c95d9bccb34852cd431ddc643
Mathieu <bon.mathieu@gmail.com>
2024-02-25 09:55:06 +0100

    Improved logic for adding custom style to TouchButton. Now only passing attributes to update or add in customStyles, not the entire StyleSheet.

--- components/TouchButton.js
+++ components/TouchButton.js
@@ -3,41 +3,48 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 export default function TouchButton({ title, press, customStyle }) {
 
-    // If customStyleSheet has been passed, use it, otherwise use the default styles
-    const buttonStyle = customStyle ? StyleSheet.create(customStyle) : styles
+    const defaultStyle = {
+        buttonText: {
+            color: '#e8b923',
+            fontSize: 15,
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+        },
+        buttonContainer: {
+            backgroundColor: '#2b50c8',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingHorizontal: 15,
+            borderRadius: 9,
+            height: 50,
+            alignSelf: 'center',
+            margin: 5,
+            borderWidth: 2,
+            borderColor: '#e8b923',
+            elevation: 50,
+    
+        }
+    }
 
+// If cutomStyle is passed, merge it with the defaultStyle
+    if (customStyle) {
+        defaultStyle.buttonText = { ...defaultStyle.buttonText, ...customStyle.buttonText }
+        defaultStyle.buttonContainer = { ...defaultStyle.buttonContainer, ...customStyle.buttonContainer }
+    }
+    
+    const styles = StyleSheet.create(defaultStyle)
+
+    
     return (
         <View>
             <TouchableOpacity
-                style={buttonStyle.buttonContainer}
+                style={styles.buttonContainer}
                 onPress={press}
             >
-                <Text style={buttonStyle.buttonText}>{title}</Text>
+                <Text style={styles.buttonText}>{title}</Text>
             </TouchableOpacity>
         </View>
     )
 }
 
 
-const styles =  StyleSheet.create({
-    buttonText: {
-        color: '#e8b923',
-        fontSize: 15,
-        fontWeight: 'bold',
-        textTransform: 'uppercase',
-    },
-    buttonContainer: {
-        backgroundColor: '#2b50c8',
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingHorizontal: 15,
-        borderRadius: 9,
-        height: 50,
-        alignSelf: 'center',
-        margin: 5,
-        borderWidth: 2,
-        borderColor: '#e8b923',
-        elevation: 50,
-
-    }
-})
