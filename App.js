e140a4b0b9eca0b5ab1de6c6049f7e010581d88d
norah <norah.essaih@gmail.com>
2024-02-27 18:45:54 +0100

    Préparation avant fusion avec dev : ajout et mise à jour des fichiers

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import HomeScreen from './screens/HomeScreen'
+import SplashScreen from './screens/SplashScreen'
 import GameScreen from './screens/GameScreen'
 import SettingsScreen from './screens/SettingsScreen'
 import ScoresScreen from './screens/ScoresScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import PlayerScreen from './screens/PlayerScreen'
+
+
 
 
 const Stack = createNativeStackNavigator()
@@ -19,8 +22,8 @@ export default function App() {
     <NavigationContainer>
 
       <Stack.Navigator>
-
-        {/* <Stack.Screen name='HomeScreen' component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='Splashscreen' component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name='HomeScreen' component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name='RegisterScreen' component={RegisterScreen} options={{ headerShown: false }} /> */}
         <Stack.Screen name='PlayerScreen' component={PlayerScreen} options={{ headerShown: false }} />
         <Stack.Screen name='GameScreen' component={GameScreen} options={{ headerShown: false }} />
---
+++ screens/SplashScreen.js
@@ -0,0 +1,31 @@
+import { View, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+export default function SplashScreen({navigation}) {
+
+  const handleAnimationFinish = () => {
+
+    navigation.navigate('HomeScreen');
+  };
+
+  return (
+    
+    <View style={styles.container}>
+      <LottieView
+        style={{ flex: 1, width: '100%' }}
+        source={require('../assets/Animations/lottie.json')}
+        autoPlay
+        loop={false} 
+        onAnimationFinish={handleAnimationFinish} 
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
