e18b8d718b9fdf734baf7eb4f53b57d223db33b5
norah <norah.essaih@gmail.com>
2024-03-10 16:19:32 +0100

    ajout couleur pour rendre visible le placeHolder sur iOS

--- components/TextField.js
+++ components/TextField.js
@@ -14,6 +14,7 @@ export default function TextField({
         <TextInput
             style={styles.field}
             placeholder={placeholder}
+            placeholderTextColor="#000" 
             value={value}
             onChangeText={onChangeText}
             autoCorrect={false}
