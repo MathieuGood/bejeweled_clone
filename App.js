64d55f2e085026ee41293d1020867bd5f7ac8e87
norah <norah.essaih@gmail.com>
2024-03-10 16:21:01 +0100

    ajout du componsant  font dans routeur

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import GameScreen from './screens/GameScreen'
 import SettingsScreen from './screens/SettingsScreen'
 import HomeScreen from './screens/HomeScreen'
-import SplashScreen from './screens/SplashScreen';
+import SplashScreen from './screens/SplashScreen'
 import ThemePreloader from './components/preloaderComponent/ThemesPreloader'
+import FontLoader from './components/preloaderComponent/FontLoader'
 import { AppProvider } from './providers/AppContext'
 
 
@@ -17,18 +18,18 @@ export default function App() {
   return (
     <AppProvider>
         <ThemePreloader />
+         <FontLoader/>
         
-    <NavigationContainer>
+           <NavigationContainer>
 
-      <Stack.Navigator>
-        <Stack.Screen name='SplashScreen' component={SplashScreen} options={{ headerShown: false }} />
-        <Stack.Screen name='HomeScreen' component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name='GameScreen' component={GameScreen} options={{ headerShown: false }} />
-        <Stack.Screen name='SettingsScreen' component={SettingsScreen} options={{ headerShown: false }} />
-      </Stack.Navigator>
+            <Stack.Navigator>
+              {/* <Stack.Screen name='SplashScreen' component={SplashScreen} options={{ headerShown: false }} /> */}
+              <Stack.Screen name='HomeScreen' component={HomeScreen} options={{ headerShown: false }} />
+              <Stack.Screen name='GameScreen' component={GameScreen} options={{ headerShown: false }} />
+              <Stack.Screen name='SettingsScreen' component={SettingsScreen} options={{ headerShown: false }} />
+            </Stack.Navigator>
 
-    </NavigationContainer>
-
+          </NavigationContainer>
     </AppProvider>
 
   );
