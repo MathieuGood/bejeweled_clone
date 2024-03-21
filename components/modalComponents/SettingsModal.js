886e6ad3e34c7e63739c2fa0ea0c5e72e380e416
Mathieu <bon.mathieu@gmail.com>
2024-03-21 22:52:51 +0100

    Fix bug in switching to Giza theme when user not logged in

--- components/modalComponents/SettingsModal.js
+++ components/modalComponents/SettingsModal.js
@@ -4,7 +4,7 @@ import CustomModal from './CustomModal'
 import AppContext from '../../providers/AppContext'
 import IconButton from '../IconButton'
 
-import {Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { appThemes } from '../../themes/appThemes'
 import ThemeCard from '../ThemeCard'
 
@@ -30,52 +30,59 @@ export default function SettingsModal({
             changeModalVisible={changeModalVisible}
         >
 
-            <SubHead title='Theme'/>
+            <SubHead title='Theme' />
 
             <Text style={styles.chooseThemeMessage}>
-               Please select a theme to customize your experience.
+                Please select a theme to customize your experience.
             </Text>
 
             <View >
                 <ThemeCard
                     iconName="image-plus"
-                    title='Jewels'               
+                    title='Jewels'
                     onPress={() => {
-                        setTheme('detailed')}}
+                        setTheme('detailed')
+                    }}
                     isSelected={theme === 'detailed'}
                     imageSource={Image1}
                 />
 
-            
+
                 <ThemeCard
                     iconName="image-plus"
-                    title="Pyramid of Giza"              
+                    title="Pyramid of Giza"
                     onPress={() => {
-                        navigation.navigate('HomeScreen', { player_id: route.params.player_id, player_name: route.params.player_name })
+                        if (route.params === undefined || route.params.player_id === undefined || route.params.player_name === undefined) {
+                            navigation.navigate('HomeScreen')
+                        }
+                        else {
+                            navigation.navigate('HomeScreen', { player_id: route.params.player_id, player_name: route.params.player_name })
+                        }
                         setTheme('flatDesign')
                     }}
                     isSelected={theme === 'flatDesign'}
                     imageSource={Image2}
                 />
 
-                    <ThemeCard
+                <ThemeCard
                     iconName="image-plus"
                     title="Upload Image"
                     onPress={() => {
-                        addImage()}}
+                        addImage()
+                    }}
                     isSelected={theme === 'customImage'}
-                    />
-    
-              </View>
+                />
 
-              <View style={{position:'absolute', top:-26, right:-37}}>
-                        <IconButton 
-                            iconName="close"
-                            iconSize={39}
-                            press={() => {
-                                changeModalVisible(false);
-                            }}
-                        />
+            </View>
+
+            <View style={{ position: 'absolute', top: -26, right: -37 }}>
+                <IconButton
+                    iconName="close"
+                    iconSize={39}
+                    press={() => {
+                        changeModalVisible(false);
+                    }}
+                />
 
             </View>
         </CustomModal>
@@ -83,12 +90,11 @@ export default function SettingsModal({
 
 }
 const styles = StyleSheet.create({
-      chooseThemeMessage: {
+    chooseThemeMessage: {
         textAlign: 'center',
-        fontSize: 20, 
-        color: '#757575', 
-        paddingHorizontal: 5, 
-        fontFamily:'Assassin'
-      }
-  })
-  
+        fontSize: 20,
+        color: '#757575',
+        paddingHorizontal: 5,
+        fontFamily: 'Assassin'
+    }
+})
