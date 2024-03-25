e2a13d722c2d33cf0a13789ed427298b7583de19
norah <norah.essaih@gmail.com>
2024-03-25 17:42:36 +0100

    ajout import platform

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Platform } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
